ac87b5026921d5d39acc72aa4028f0aec898e7e3
Oliver Muthomi <oliver.mwitia@gmail.com>
2019-04-18 00:33:49 +0300

    Create inputs controller

---
+++ app/src/main/java/com/example/conductorexample/InputsController.java
@@ -0,0 +1,27 @@
+package com.example.conductorexample;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.bluelinelabs.conductor.Controller;
+
+public class InputsController extends Controller {
+
+    @Override
+    protected View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
+        View view = inflater.inflate(R.layout.controller_inputs, container, false);
+
+        // Views
+        EditText et1 = view.findViewById(R.id.et1);
+        EditText et2 = view.findViewById(R.id.et2);
+        EditText btn = view.findViewById(R.id.btn);
+
+        return view;
+    }
+
+}
--- app/src/main/java/com/example/conductorexample/MainActivity.java
+++ app/src/main/java/com/example/conductorexample/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.conductorexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.bluelinelabs.conductor.Conductor;
+import com.bluelinelabs.conductor.Router;
+import com.bluelinelabs.conductor.RouterTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declare router
+    private Router router;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Controller Container
+        ViewGroup container = (ViewGroup) findViewById(R.id.container);
+
+        // Initialize router and attach controller
+        router = Conductor.attachRouter(this, container, savedInstanceState);
+        if (!router.hasRootController()) {
+            router.setRoot(RouterTransaction.with(new InputsController()));
+        }
     }
 }
