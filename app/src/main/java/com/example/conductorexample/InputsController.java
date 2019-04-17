42a57b0e7c99b5d46f8e5efd30c6b39b7bbddb16
Oliver Muthomi <oliver.mwitia@gmail.com>
2019-04-18 02:22:44 +0300

    Pass data between controllers

--- app/src/main/java/com/example/conductorexample/InputsController.java
+++ app/src/main/java/com/example/conductorexample/InputsController.java
@@ -1,10 +1,12 @@
 package com.example.conductorexample;
 
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -28,7 +30,8 @@ public class InputsController extends Controller {
         return view;
     }
 
-    private void doCalculation(Integer valueOf1, Integer valueOf2) {
+    private void doCalculation(int valueOf1, int valueOf2) {
+
         // Calculations
         int sum = valueOf1 + valueOf2;
         int product = valueOf1 * valueOf2;
@@ -43,7 +46,7 @@ public class InputsController extends Controller {
 
         // Send to output Controller
         RouterTransaction routerTransaction = RouterTransaction
-                .with(new OutputController())
+                .with(new OutputController(sum, product, quotient, difference))
                 .popChangeHandler(new HorizontalChangeHandler())
                 .pushChangeHandler(new HorizontalChangeHandler());
 
--- app/src/main/java/com/example/conductorexample/OutputController.java
+++ app/src/main/java/com/example/conductorexample/OutputController.java
@@ -1,9 +1,11 @@
 package com.example.conductorexample;
 
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -11,6 +13,20 @@ import com.bluelinelabs.conductor.Controller;
 
 public class OutputController extends Controller {
 
+    public OutputController(int sum, int product, int quotient, int difference) {
+
+        // Prepare bundle to pass parameters
+        getArgs().putInt("sum", sum);
+        getArgs().putInt("product", product);
+        getArgs().putInt("quotient", quotient);
+        getArgs().putInt("difference", difference);
+    }
+
+    public OutputController(){
+
+    }
+
+    @NonNull
     @Override
     protected View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
         View view = inflater.inflate(R.layout.controller_outputs, container, false);
@@ -20,6 +36,16 @@ public class OutputController extends Controller {
         TextView tvDifference = view.findViewById(R.id.diff);
         TextView tvProduct = view.findViewById(R.id.product);
         TextView tvQuotient = view.findViewById(R.id.quotient);
+
+        // Retrieve parameters from passed bundle
+        Bundle bundle = getArgs();
+        Toast.makeText(getActivity(), getArgs().getInt("sum")+"", Toast.LENGTH_SHORT).show();
+
+        // Populate views
+        tvSum.setText(String.valueOf(bundle.getInt("sum")));
+        tvProduct.setText(String.valueOf(bundle.getInt("product")));
+        tvQuotient.setText(String.valueOf(bundle.getInt("quotient")));
+        tvDifference.setText(String.valueOf(bundle.getInt("difference")));
 
         return view;
     }
