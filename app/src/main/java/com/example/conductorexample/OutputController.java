1064a5eb149a9be6a26840839106d385efda4a1f
Oliver Muthomi <oliver.mwitia@gmail.com>
2019-04-18 01:19:10 +0300

    Create output controller

--- app/src/main/java/com/example/conductorexample/OutputController.java
+++ app/src/main/java/com/example/conductorexample/OutputController.java
@@ -3,29 +3,25 @@ package com.example.conductorexample;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 import com.bluelinelabs.conductor.Controller;
 
-public class InputsController extends Controller {
+public class OutputController extends Controller {
 
     @Override
     protected View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
         View view = inflater.inflate(R.layout.controller_inputs, container, false);
 
         // Views
-        EditText et1 = view.findViewById(R.id.et1);
-        EditText et2 = view.findViewById(R.id.et2);
-        Button btn = view.findViewById(R.id.btn);
-
+        TextView tvSum = view.findViewById(R.id.sum);
+        TextView tvDifference = view.findViewById(R.id.diff);
+        TextView tvProduct = view.findViewById(R.id.product);
+        TextView tvQuotient = view.findViewById(R.id.quotient);
 
         return view;
     }
-
-
-
 
 }
